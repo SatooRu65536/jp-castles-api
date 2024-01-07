9338032395017774ffe3d9858cd53b43a4d7bb49
SatooRu65536 <87361015+SatooRu65536@users.noreply.github.com>
2024-01-08 01:07:28 +0900

    mv: フォルダ構造を変更

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "drizzle-kit";
 
 export default {
-  schema: "./src/schema.ts",
+  schema: "./src/models/schema.ts",
   out: "./drizzle/migrations",
   driver: "d1",
   dbCredentials: {
